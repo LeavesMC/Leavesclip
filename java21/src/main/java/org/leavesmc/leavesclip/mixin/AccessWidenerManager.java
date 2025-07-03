fbc58366ff64bac92dfd12bdc667db5325253a19
AlexLiuDev233 <wzylin11@outlook.com>
2025-07-03 10:33:09 +0800

    fix: if AccessWidener is empty, do not try to load

--- java21/src/main/java/org/leavesmc/leavesclip/mixin/AccessWidenerManager.java
+++ java21/src/main/java/org/leavesmc/leavesclip/mixin/AccessWidenerManager.java
@@ -22,6 +22,7 @@ public class AccessWidenerManager {
     public static void initAccessWidener(URLClassLoader classLoader) {
         AccessWidenerReader reader = new AccessWidenerReader(instance);
         for (String config : MixinJarResolver.accessWidenerConfigs) {
+            if (config.isEmpty() || config == null) continue;
             applyAccessWidenerConfig(classLoader, config, reader);
         }
     }
